aab77725a0de2ee2e81b808ecd5c295cb16cc9ca
moamel <75712100+moaml1999@users.noreply.github.com>
2023-07-04 15:48:07 +0300

    Add files via upload

---
+++ Telegram pico W/secrets.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+# Secret Keys.
+
+secrets = {
+    "ssid"                     : "******",
+    "password"                 : "******",
+    "telegram_bot_token"       : "*******************************",
+}
