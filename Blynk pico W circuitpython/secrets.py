4b20fb78afb6f06238a6d0cf7f37cdde2703f7de
moamel <75712100+moaml1999@users.noreply.github.com>
2023-07-26 01:30:26 +0300

    Delete Blynk pico W circuitpython directory

--- Blynk pico W circuitpython/secrets.py
+++
@@ -1,8 +0,0 @@
-# Write your code here :-)
-# Secret Keys.
-
-secrets = {
-    "ssid"                     : "🙂",
-    "password"                 : "90201897",
-    "telegram_bot_token"       : "5949756428:AAGwAtCeuKiQ-AGsTCye7dsvGhtdGdeS2_s",
-}
